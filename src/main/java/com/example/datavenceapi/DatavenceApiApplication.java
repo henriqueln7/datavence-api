a96645cc15fe073d55976856acdf0be2cfee0087
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-06-28 23:49:22 -0300

    Initial commit

---
+++ src/main/java/com/example/datavenceapi/DatavenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.datavenceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatavenceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatavenceApiApplication.class, args);
+    }
+
+}
